bbd6e4eb0f5883f9ea86b5c8331e83b208b67d9b
bezustally <63110847+bezustally@users.noreply.github.com>
2020-08-05 18:49:05 +0500

    Update PasswordCheckerGUI.py

--- PasswordCheckerGUI.py
+++ PasswordCheckerGUI.py
@@ -23,7 +23,6 @@ def pwned_api_check(password):
 	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
 	first5_char, tail = sha1password[:5], sha1password[5:]
 	response = request_api_data(first5_char)
-	print(first5_char, tail)
 	return get_password_leaks_count(response, tail)
 
 
